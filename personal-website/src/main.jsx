b2559dc2895fad630f4cf003ffef8c3850fe79ea
Isaac Man <103552680+haydayisawsome@users.noreply.github.com>
2025-09-07 21:15:54 -0400

    first commit

---
+++ personal-website/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+const root = createRoot(document.getElementById("root"))
+
+import App from "/src/components/App.jsx"
+
+root.render(
+  <>
+    <App />
+  </>
+)
